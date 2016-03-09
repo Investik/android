f9e39c40a1e2daa8dbb6ae0ece867bbea63418a4
Investik <alexander.meleshkin@mail.ru>
2016-03-09 09:55:55 +0400

    Added files via upload

---
+++ workout-Fragments-handler-stopwatch-child-fragment-tablet-and-phone/java/WorkoutListFragment.java
@@ -0,0 +1,45 @@
+package com.example.alex.workout;
+
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+    static interface WorkoutListener{
+        void itemClicked(long id);
+    };
+    private WorkoutListener listener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String [] names = new String[Workout.workouts.length];
+            for (int i=0; i<names.length; i++)
+                names[i]=Workout.workouts[i].getName();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onAttach (Activity activity){
+        super.onAttach(activity);
+        this.listener = (WorkoutListener) activity;
+    }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id){
+        if(listener!=null)
+            listener.itemClicked(id);
+    }
+
+}
